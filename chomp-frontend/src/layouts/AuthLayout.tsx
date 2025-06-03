2d5d9340efdf80f838e33e32901fb8b956dd8273
Oog <79869964+DanielShandley@users.noreply.github.com>
2025-06-03 23:18:40 +0100

    Update chomp-frontend/src/layouts/AuthLayout.tsx
    Co-authored-by: Charlotte <charlie@lavatheif.com>

--- chomp-frontend/src/layouts/AuthLayout.tsx
+++ chomp-frontend/src/layouts/AuthLayout.tsx
@@ -2,7 +2,7 @@ import { Footer } from "../features/footer/Footer";
 
 export const AuthLayout = ({ children }: { children: React.ReactNode }) => (
   <div className="flex flex-col min-h-screen">
-    <main className="flex-grow">{children}</main>
+    <Outlet className="flex-grow" />
     <Footer />
   </div>
 );
